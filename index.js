eb2f6685801fecf3e817d1b46c06d4c683020974
shreshth0007 <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-08-18 10:16:25 +0530

    commit changes

--- index.js
+++ index.js
@@ -1,70 +1,101 @@
-// console.log("Hello World");
+// // console.log("Hello World");
 
-// Function Declaration
+// // Function Declaration
 
-function sum(a, b) {
-    return a + b;
-}
+// function sum(a, b) {
+//     return a + b;
+// }
 
-function sqSum(a, b) {
-    return Math.sqrt(a) + Math.sqrt(b);
-}
+// function sqSum(a, b) {
+//     return Math.sqrt(a) + Math.sqrt(b);
+// }
 
-console.log(sum(2, 3));
-console.log(sqSum(4, 9));
-
-
-// Function as Expression
-
-const sumExp = function (a, b) {
-    return a + b;
-};
-
-console.log(sumExp(10, 20));
+// console.log(sum(2, 3));
+// console.log(sqSum(4, 9));
 
 
-// Arrow Function
+// // Function as Expression
 
-const sumArrow = (a, b) => {
-    return a + b;
-};
+// const sumExp = function (a, b) {
+//     return a + b;
+// };
 
-console.log(sumArrow(5, 6));
-
-
-// IIFE (Immediately Invoked Function Expression)
-
-(() => {
-    console.log("Heyy... using IIFE");
-})();
+// console.log(sumExp(10, 20));
 
 
-// Callback Function
+// // Arrow Function
 
-function showResult(callback, msg) {
-    const result = callback(40, 50);
-    console.log(msg + " Your result is: " + result);
-}
+// const sumArrow = (a, b) => {
+//     return a + b;
+// };
 
-showResult(sum, "Hi Rahul!");
+// console.log(sumArrow(5, 6));
 
 
-// Callback Example
+// // IIFE (Immediately Invoked Function Expression)
 
-function login(error, msg) {
-    if (error) {
-        console.log("Error: " + error);
-    } else {
-        console.log(msg);
-    }
-}
+// (() => {
+//     console.log("Heyy... using IIFE");
+// })();
 
-function loginHandler(username, password, callback) {
-    if (username === "shreshth" && password === "123") {
-        callback(null, "Login Successful");
-    } else {
-        callback("Username or Password is Incorrect");
-    }
-}
 
-loginHandler("shreshth", "123", login);
+// // Callback Function
+
+// function showResult(callback, msg) {
+//     const result = callback(40, 50);
+//     console.log(msg + " Your result is: " + result);
+// }
+
+// showResult(sum, "Hi Rahul!");
+
+
+// // Callback Example
+
+// function login(error, msg) {
+//     if (error) {
+//         console.log("Error: " + error);
+//     } else {
+//         console.log(msg);
+//     }
+// }
+
+// function loginHandler(username, password, callback) {
+//     if (username === "shreshth" && password === "123") {
+//         callback(null, "Login Successful");
+//     } else {
+//         callback("Username or Password is Incorrect");
+//     }
+// }
+
+// loginHandler("shreshth", "123", login);
+
+
+// function ping(){
+//     alert("chua kaise be");
+// }
+
+document.getElementById("buildResume").addEventListener("click", function () {
+
+    const name = "Shreshth Mittal";
+    const email = "shreshth@example.com";
+    const phone = "9876543210";
+    const skills = ["Java", "Python", "HTML", "CSS", "JavaScript"];
+
+    document.getElementById("resume").innerHTML = 
+     "<h1>" + name + "</h1>" +
+     "<br>"+
+        "<p>Email: " + email + "</p>" +
+        "<p>Phone: " + phone + "</p>" +
+        "<br>"+
+        "<h3>Skills</h3>" +
+        "<p>Java, Python, HTML, CSS, JavaScript</p>" +
+        "<br>"+
+        "<h3>Education</h3>" +
+        "<p>B.Tech Computer Science & Engineering</p>" +
+        "<br>"+
+        "<h3>Projects</h3>" +
+        "<p>Weather Forecast App</p>" +
+        "<p>Smart Resume Viewer</p>"+
+        "<p>Fake News Detector</p>"+
+        "<p>SafeSphere AI</p>";
+});
